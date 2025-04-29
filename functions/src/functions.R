8874fe848a0d04f210388b1f264bf99db41a6d6d
aebuechele29 <amandabuechele2029@u.northwestern.edu>
2025-04-29 08:58:36 -0500

    Add functions

--- functions/src/functions.R
+++ functions/src/functions.R
@@ -1,1 +1,27 @@
-# This file is intentionally left blank.
+efficient_min <- function(x, na.rm = TRUE) {
+
+  if (all(is.na(x))) return(x[1])
+  return(min(x, na.rm = na.rm))
+
+}
+
+efficient_max <- function(x, na.rm = TRUE) {
+
+  if (all(is.na(x))) return(x[1])
+  return(max(x, na.rm = na.rm))
+
+}
+
+efficient_mean <- function(x, na.rm = TRUE) {
+
+  if (all(is.na(x))) return(x[1])
+  return(mean(x, na.rm = na.rm))
+
+}
+
+efficient_sum <- function(x, na.rm = TRUE) {
+
+  if (all(is.na(x))) return(x[1])
+  return(sum(x, na.rm = na.rm))
+
+} This file is intentionally left blank.
